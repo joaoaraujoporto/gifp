9a93f118f0ad8ed55d36a3be73b2fc8c76e25888
joao.porto <joaov.aporto@gmail.com>
2019-09-13 21:57:59 -0300

    GMP basics done

--- leargmp/leargmp.c
+++ leargmp/leargmp.c
@@ -2,16 +2,15 @@
 #include "stdarg.h"
 #include "gmp.h"
 
-typedef mpz_t integer;
-
-
-
+#define integer mpz_t
 #define init_i __gmpz_init
 #define inits_i __gmpz_inits
-#define add_i __gmpz_add
+#define sum_i mpz_add
 #define clear_i __gmpz_clear
 #define clears_i __gmpz_clears
 #define set_i __gmpz_set_ui
+#define set_s __gmpz_set_str
+#define init_set_i __gmpz_init_set_ui
 
 // void init_i(mpz_t x) { mpz_init(x); }
 // void clear_i(mpz_t x) { mpz_clear(x); }
@@ -22,10 +21,10 @@ int main() {
     inits_i(n, m, sum, NULL);
 
     set_i(n, 1);
-    set_i(m, 1);\
-    add_i(sum, n, m);
+    mpz_set_str(m, "900000000000000000000000000000000000000000000000000000000000000000", 0);
+    sum_i(sum, n, m);
 
-    printf("hello\n");
+    gmp_printf("n + m = %Zd\n", sum);
 
     clears_i(n, m, sum, NULL);
 }
